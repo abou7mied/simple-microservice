7fb8c6575c4c4b7c7f56e4562f90e9a2a1b7ad65
Ahmed Sabry <abou7mied.soft@gmail.com>
2019-08-07 09:14:55 +0200

    Add log for server starting

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const controllers = require('./controllers');
 
+const PORT = process.env.PORT || 7000;
 const app = express();
 
 controllers.init(app);
-app.listen(process.env.PORT || 7000);
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+});
 
 
